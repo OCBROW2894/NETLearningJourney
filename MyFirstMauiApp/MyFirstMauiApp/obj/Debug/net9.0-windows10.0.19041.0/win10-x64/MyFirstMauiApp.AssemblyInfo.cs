bd04efbf673d9067a097ef17d196ea7c0d4159ba
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-04-07 12:42:00 +0300

    .NET MAUI!

--- MyFirstMauiApp/MyFirstMauiApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MyFirstMauiApp.AssemblyInfo.cs
+++ MyFirstMauiApp/MyFirstMauiApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MyFirstMauiApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8047cffbb661979fa4b00d815795c03e9173adc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0ba998bb796bf5a377fd4f20f1e355691859420c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
