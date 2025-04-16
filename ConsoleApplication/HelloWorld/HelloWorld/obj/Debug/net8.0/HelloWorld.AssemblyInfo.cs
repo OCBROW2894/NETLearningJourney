e6f185bd17a644aacfc46ae3fcbf7dd812e0f92b
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-04-16 06:58:59 +0300

    Wrote first lines of code

--- ConsoleApplication/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6974540d2fd204b62d9afc8eb143e04eba46e40c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934991c7deb42a5c1f828202b0dd576fff678cbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
