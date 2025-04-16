7c29574e34c9e324f380a62f8bff73e3505747df
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-04-16 07:03:15 +0300

    Write and writeLine

--- ConsoleApplication/HelloWorld/HelloWorld/Program.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-Console.Write("Congratulations!");
-Console.Write(" ");
+Console.WriteLine("Congratulations!");
 Console.Write("You Wrote your first lines of code.");
