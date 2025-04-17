a26c713f64e0082d6358201143e2075407a35052
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-04-17 13:29:57 +0300

    Display two messages

--- ConsoleApplication/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6f185bd17a644aacfc46ae3fcbf7dd812e0f92b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c29574e34c9e324f380a62f8bff73e3505747df")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
