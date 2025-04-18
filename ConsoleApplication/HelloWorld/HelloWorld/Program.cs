f04460127008debfd852479ce405058bf67904cc
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-04-18 16:00:58 +0300

    Format output using character escape sequences

--- ConsoleApplication/HelloWorld/HelloWorld/Program.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+/*
 Console.WriteLine("Hello\nWorld!"); // New line
 Console.WriteLine("Hello\tWorld!"); // Tab
 Console.WriteLine("Hello \"World\"!"); // Quotes
 Console.WriteLine("c:\\source\\repos"); // Backslash
+*/
+
+Console.WriteLine("Generating invoices for Customer \"Consto Corp\"... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
