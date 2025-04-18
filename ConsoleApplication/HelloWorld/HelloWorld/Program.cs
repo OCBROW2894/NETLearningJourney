e40dabc1ff8e6156c35430a4799ef2eb1d599596
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-04-18 13:41:14 +0300

    Character escape sequences

--- ConsoleApplication/HelloWorld/HelloWorld/Program.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string userName = "Bob";
-int numberOfMessages = 3;
-double temperature = 34.4;
-
-Console.Write("Hello, " + userName + "! " + "You have " + numberOfMessages + " messages in your inbox. The temperature is " + temperature + " celsius.");
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
--- ConsoleApplication/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a0cbdf2a16fd413cfc2ef48acd37d2c0f3eaa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd406e43514328374cab268368c873c12b078c52")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
