a1dc4960b84d234a1a72637d2147862c10825368
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-04-18 15:25:58 +0300

    Quotes

--- ConsoleApplication/HelloWorld/HelloWorld/Program.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello\nWorld!");
-Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("Hello\nWorld!"); // New line
+Console.WriteLine("Hello\tWorld!"); // Tab
+Console.WriteLine("Hello \"World\"!"); // Quotes
--- ConsoleApplication/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd406e43514328374cab268368c873c12b078c52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e40dabc1ff8e6156c35430a4799ef2eb1d599596")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
