c677288bd2b70c4074f0b074aff8e8c9ef1b1975
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-04-18 12:46:02 +0300

    Initializing the variable

--- ConsoleApplication/HelloWorld/HelloWorld/Program.cs
+++ ConsoleApplication/HelloWorld/HelloWorld/Program.cs
@@ -1,11 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string firstName;
-firstName = "Tendo";
-Console.WriteLine(firstName);
-firstName = "Nagato";
-Console.WriteLine(firstName);
-firstName = "Isabella";
-Console.WriteLine(firstName);
-firstName = "Yasmin";
-Console.WriteLine(firstName);
+string firstName = "Tendo";
+string lastName = "Pain";
+Console.Write(firstName);
+Console.WriteLine(" "+lastName);
